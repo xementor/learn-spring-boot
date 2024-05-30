6e8f90aedab9d0f1803da526fad28e5b4d7dd2e2
Zonaid <ahammedzonaid@gmail.com>
2024-05-30 20:20:46 +0600

    initial commit

---
+++ src/test/java/com/vong/cong/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vong.cong.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
